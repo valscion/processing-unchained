d0e3528dd4795f1274beed1fa27dc22e23f090e8
Aarn <pelto.karvanen@gmail.com>
2013-12-11 13:02:50 +0200

    Koitan saada pelaajan maailmaan, ei käänny

--- kierros4_3d/3D_peli/src/mygame/Main.java
+++ kierros4_3d/3D_peli/src/mygame/Main.java
@@ -2,19 +2,44 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.InputListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
-
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.ZipLocator;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 /**
  * test
  * @author normenhansen
@@ -23,21 +48,69 @@ public class Main extends SimpleApplication {
     private Spatial sceneModel;
     private RigidBodyControl landscape;
     private BulletAppState bulletAppState;
+    //näppäimille
+    private boolean left = false, right = false, up = false, down = false, keyE = false;
+    private CharacterControl player;
+    private float xRotation = 0f;
+    //Temporary vectors used on each frame.
+    //They here to avoid instanciating new vectors on each frame
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+    private Vector3f walkDirection = new Vector3f();
     
-
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
     }
 
+
     @Override
     public void simpleInitApp() { 
+        this.initPhysics();
+        this.initMaze();
+        this.initPlayer();
+        this.initLights();
+        // We re-use the flyby camera for rotation, while positioning is handled by physics
+        viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+        flyCam.setMoveSpeed(100);
+        setUpKeys();
+    }
+
+    private void initLights(){
+            /** A white ambient light source. */ 
+            AmbientLight ambient = new AmbientLight();
+            ambient.setColor(ColorRGBA.White);
+            rootNode.addLight(ambient); 
+    }
+    
+    private void initPhysics(){
         /** Set up Physics */
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
         //tässä oli kommentit että debugi pois päältä
         bulletAppState.getPhysicsSpace().enableDebug(assetManager);
-        
+    }
+    
+      /** We over-write some navigational key mappings here, so we can
+   * add physics-controlled walking and jumping: */
+  private void setUpKeys() {
+    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addMapping("RotateWorld", new KeyTrigger(KeyInput.KEY_E));
+    inputManager.addListener(this, "Left");
+    inputManager.addListener(this, "Right");
+    inputManager.addListener(this, "Up");
+    inputManager.addListener(this, "Down");
+    inputManager.addListener(this, "Jump");
+    inputManager.addMapping("RotateWorld", new KeyTrigger(KeyInput.KEY_E));
+    inputManager.addListener(this, "RotateWorld");
+
+  }
+    
+    private void initMaze(){
         sceneModel = assetManager.loadModel("Scenes/tyhjaMantu.j3o");
         sceneModel.setLocalScale(1f);//vähän turha ilmottaa 
         CollisionShape sceneShape = CollisionShapeFactory.createMeshShape((Node) sceneModel);
@@ -45,28 +118,72 @@ public class Main extends SimpleApplication {
         sceneModel.addControl(landscape);
         rootNode.attachChild(sceneModel);
         bulletAppState.getPhysicsSpace().add(landscape);
-        
-
-    /** A white ambient light source. */ 
-    AmbientLight ambient = new AmbientLight();
-    ambient.setColor(ColorRGBA.White);
-    rootNode.addLight(ambient); 
-        /*
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);*/
     }
-
-    @Override
+    
+    private void initPlayer(){
+            // We set up collision detection for the player by creating
+    // a capsule collision shape and a CharacterControl.
+    // The CharacterControl offers extra settings for
+    // size, stepheight, jumping, falling, and gravity.
+    // We also put the player in its starting position.
+    CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+    player = new CharacterControl(capsuleShape, 0.05f);
+    player.setJumpSpeed(20);
+    player.setFallSpeed(30);
+    player.setGravity(30);
+    player.setPhysicsLocation(new Vector3f(0, 10, 0));
+    }
+    
+   /** These are our custom actions triggered by key presses.
+   * We do not walk yet, we just keep track of the direction the user pressed. */
+  public void onAction(String binding, boolean isPressed, float tpf) {
+    if (binding.equals("Left")) {
+      left = isPressed;
+    } else if (binding.equals("Right")) {
+      right= isPressed;
+    } else if (binding.equals("Up")) {
+      up = isPressed;
+    } else if (binding.equals("Down")) {
+      down = isPressed;
+    } else if (binding.equals("Jump")) {
+      if (isPressed) { player.jump(); }
+    } else if(binding.equals("RotateWorld")) {
+        this.rotateWorld(0.1f);
+    }
+  }
+  
+  @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
     }
-
+  
+  /*Pyöräyttää landscapea
+   * 
+   */
+  private void rotateWorld(float rotationFloat){
+      xRotation += rotationFloat;
+      if (xRotation > 2){
+          xRotation = 0;
+      }
+      landscape.setPhysicsRotation(new Quaternion(0f,-xRotation/8,xRotation/3,0f));
+  }    
+    
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
