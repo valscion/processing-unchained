7839b155f58ac31c6d378074e813d6af565cabba
Vesa Laakso <vesa.laakso@venuu.fi>
2013-12-12 15:00:00 +0200

    Pois käyttämättömät importit

--- kierros4_3d/3D_peli/src/mygame/Main.java
+++ kierros4_3d/3D_peli/src/mygame/Main.java
@@ -1,20 +1,5 @@
 package mygame;
 
-import com.jme3.app.SimpleApplication;
-import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
-import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.control.CharacterControl;
-import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.bullet.util.CollisionShapeFactory;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.InputListener;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.light.AmbientLight;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.app.SimpleApplication;
